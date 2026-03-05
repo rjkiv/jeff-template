7c53fee15902e982996f2b692720972c2ddb9c14
First Last <69217234+ieee802dot11ac@users.noreply.github.com>
2026-03-04 19:58:05 -0800

    Update compilers.zip version round 2

--- configure.py
+++ configure.py
@@ -158,7 +158,7 @@ if not config.non_matching:
 
 # Tool versions
 config.binutils_tag = "2.42-1"
-config.compilers_tag = "20260304"
+config.compilers_tag = "20260305"
 config.dtk_tag = "v0.3.0"
 config.objdiff_tag = "v3.2.1"
 config.sjiswrap_tag = "v1.2.1"
